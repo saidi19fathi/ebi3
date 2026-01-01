4d15dad7d5d59ee88778a2f55512abadeaece100
saidi19fathi <tuniserv.visiteurs@gmail.com>
2026-01-01 08:05:24 +0000

    Modifications du [date] - [description des changements]

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,17 @@ from .models import User, UserProfile
 class CustomUserCreationForm(UserCreationForm):
     """Formulaire pour créer un nouvel utilisateur avec des champs supplémentaires"""
 
+    phone_number = forms.CharField(
+        label=_("Numéro de téléphone"),
+        required=True,
+        help_text=_("Format international : +212612345678 (recommandé) ou 00212612345678"),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': _("+212612345678"),
+            'title': _("Entrez votre numéro au format international")
+        })
+    )
+
     class Meta:
         model = User
         fields = ('username', 'email', 'role', 'phone_number',
@@ -16,6 +27,104 @@ class CustomUserCreationForm(UserCreationForm):
             'preferred_language': forms.Select(attrs={'class': 'form-control'}),
         }
 
+    def clean_phone_number(self):
+        """Valide et formate le numéro de téléphone"""
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if phone_number:
+            try:
+                # Nettoyer le numéro : supprimer les espaces, tirets, etc.
+                phone_number = phone_number.strip().replace(' ', '').replace('-', '').replace('.', '')
+
+                # Convertir les formats locaux en format international
+                if phone_number.startswith('0'):
+                    # L'utilisateur a entré un format local (ex: 0612345678)
+                    country = self.cleaned_data.get('country')
+
+                    if country:
+                        # Mapper les codes pays
+                        country_codes = {
+                            'MA': '212',  # Maroc
+                            'FR': '33',   # France
+                            'BE': '32',   # Belgique
+                            'TN': '216',  # Tunisie
+                            'DZ': '213',  # Algérie
+                            'SN': '221',  # Sénégal
+                            'CI': '225',  # Côte d'Ivoire
+                            'CM': '237',  # Cameroun
+                            'US': '1',    # États-Unis
+                            'GB': '44',   # Royaume-Uni
+                            'DE': '49',   # Allemagne
+                            'ES': '34',   # Espagne
+                            'IT': '39',   # Italie
+                            'PT': '351',  # Portugal
+                            # Ajoutez d'autres pays selon vos besoins
+                        }
+
+                        code = country_codes.get(country.code, '')
+                        if code:
+                            # Enlever le 0 initial et ajouter le code pays
+                            phone_number = f"+{code}{phone_number[1:]}"
+                        else:
+                            # Code pays non trouvé, laisser tel quel
+                            pass
+
+                # Convertir 00 en + si présent (format 00212...)
+                elif phone_number.startswith('00'):
+                    phone_number = '+' + phone_number[2:]
+
+                # S'assurer que le numéro commence par +
+                if not phone_number.startswith('+'):
+                    phone_number = '+' + phone_number
+
+                # Validation de la longueur minimale
+                if len(phone_number) < 10:  # +212612345678 = 13 caractères
+                    raise forms.ValidationError(
+                        _("Numéro de téléphone trop court. Format attendu : +212612345678")
+                    )
+
+                # Validation que le numéro ne contient que des chiffres après le +
+                if not phone_number[1:].isdigit():
+                    raise forms.ValidationError(
+                        _("Le numéro de téléphone ne doit contenir que des chiffres après le +")
+                    )
+
+                return phone_number
+
+            except Exception as e:
+                raise forms.ValidationError(
+                    _("Saisissez un numéro de téléphone valide. Format: +212612345678 ou 0612345678")
+                )
+
+        return phone_number
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Améliorer les champs existants
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': _("Choisissez un nom d'utilisateur")
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': _("votre@email.com")
+        })
+        self.fields['country'].widget.attrs.update({
+            'class': 'form-control'
+        })
+        self.fields['city'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': _("Votre ville")
+        })
+        self.fields['city'].required = False  # Rendre optionnel
+
+        # Rendre le pays obligatoire pour aider à la validation du téléphone
+        self.fields['country'].required = True
+
+        # Ajouter une aide pour le format du téléphone
+        self.fields['phone_number'].help_text += _(
+            "<br><small class='text-muted'>Exemples: +212612345678 (Maroc), +33612345678 (France), +32456123456 (Belgique)</small>"
+        )
 class CustomUserChangeForm(UserChangeForm):
     """Formulaire pour modifier un utilisateur existant"""
 
